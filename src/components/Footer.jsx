b6937b1b3e0215a15d384dd82d437cf74a302ec5
priyanshu082 <priyanshusingh216@gmail.com>
2024-02-29 23:32:58 +0530

    some chnges

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 const Footer = () => {
   return (
 
-    <div className="backgroundCursorDiv flex flex-col h-[300px] poppins2">
+    <div className="backgroundCursorDiv flex flex-col h-fit justify-center md:h-[300px]  poppins2">
 
  <div className=" flex flex-col sm:flex-row justify-around pt-[20px] sm:pt-[45px] w-[100%] border-t-[2px] border-zinc-700 mt-[20px]  h-[240px]">
-      <div className="flex flex-col w-[35%]">
-        <div className="font1 font-bold text-zinc-300 text-[25px]">
+      <div className="flex flex-col w-[90%] mx-auto md:w-[35%] md:text-left text-center">
+        <div className="font1 font-bold text-zinc-300 text-[25px] ">
           PERIODIX
         </div>
         <div className="text-zinc-400 text-[14px]">
@@ -19,7 +19,7 @@ const Footer = () => {
       </div>
 
       {/* about periodix */}
-      <div className="flex flex-col w-[40%]">
+      <div className="flex flex-col w-[90%] mx-auto  md:w-[40%] mt-[10px] md:mt-[0px] md:text-left text-center">
         <div className="font1 font-bold text-zinc-300 text-[25px]">
           ABOUT ME
         </div>
@@ -30,7 +30,7 @@ const Footer = () => {
 
     </div>
     
-    <div className=" border-zinc-500 text-zinc-300 py-[10px] text-[15px] tracking-tighter flex flex-row text-center justify-center">
+    <div className=" border-zinc-500 text-zinc-300 py-[10px] mt-[20px] text-[15px] tracking-tighter flex flex-row text-center justify-center">
             <p>Developed By Priyanshu Singh</p>
             <a href='https://github.com/priyanshu082' target="_blank" rel="noopener noreferrer" className='ml-[10px] pb-[4px]'>
   <GitHubIcon className='  w-[20px] h-[20px] text-cyan-600'/>
